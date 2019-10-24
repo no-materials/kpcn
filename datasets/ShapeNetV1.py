aa181e8d024b734f5ebcd0b9c45de377291ba13f
nomaterials <nomaterials@gmail.com>
2019-10-24 15:41:16 +0300

    cpp grid subsampling wrapper

--- datasets/ShapeNetV1.py
+++ datasets/ShapeNetV1.py
@@ -3,9 +3,21 @@ import numpy as np
 import time
 import pickle
 from sklearn.neighbors import KDTree
+
 try:
     from urllib.request import urlretrieve
 except ImportError:
     from urllib import urlretrieve
 
-#PLY IO
+# PLY IO
+from utils.ply import read_ply, write_ply
+
+# OS functions
+from os import makedirs, listdir
+from os.path import exists, join, isfile, isdir
+
+# Dataset parent class
+# from datasets.common import Dataset
+
+# Subsampling extension
+import cpp_wrappers.cpp_subsampling.grid_subsampling as cpp_subsampling
