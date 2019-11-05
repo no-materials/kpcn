a1b2309db9ac0a020542bbd2900dcec95c07e582
nomaterials <nomaterials@gmail.com>
2019-11-05 16:23:59 +0200

    Remove the -D_GLIBCXX_USE_CXX11_ABI=0 flag

--- cpp_wrappers/cpp_subsampling/setup.py
+++ cpp_wrappers/cpp_subsampling/setup.py
@@ -15,7 +15,8 @@ SOURCES = ["../cpp_utils/cloud/cloud.cpp",
 
 module = Extension(m_name,
                    sources=SOURCES,
-                   extra_compile_args=['-std=c++11'])
+                   extra_compile_args=['-std=c++11',
+                                       '-D_GLIBCXX_USE_CXX11_ABI=0'])
 
 setup(ext_modules=[module], include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
 
