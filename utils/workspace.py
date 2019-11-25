4145c946d945814eaebbaa9bdf6443c5757cc0dd
nomaterials <nomaterials@gmail.com>
2019-11-25 22:47:18 +0200

    fix ubuntu results path

--- utils/workspace.py
+++ utils/workspace.py
@@ -1,2 +1,2 @@
 # drive_results_dir = '/content/drive/My Drive/kpcn/results'
-drive_results_dir = '/media/StorageDump/drive/kpcn/results'
+drive_results_dir = '/media/stagakis/StorageDump/drive/kpcn/results'
